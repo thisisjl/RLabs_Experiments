f2ceb81cc7756545fb3e98d0f29c69e77fc8dde7
Jose Luis <joseluis.diez@upf.edu>
2015-03-06 10:53:45 +0100

    Plaid stimulus added. This is the version number 18

---
+++ Experiment01 - Plaid/trials_file.py
@@ -0,0 +1,22 @@
+numtrials = 3 					# number of trials
+# parameters for gratings 1
+
+
+mylambda1 = (100,100,100)
+duty_cycle1 = 0.2
+speed1 = 70
+orientation1 = (8,10,12)
+
+# parameters for gratings 2
+
+
+mylambda2 = (100,100,100)
+duty_cycle2 = 0.2
+speed2 = -70
+orientation2 = (-8,-10,-12)
+
+#other parameters
+timeCurrentTrial = 5 	# time for each trial
+
+# b_color = [(0.0,0.88,0.88),(0.88,0,0),(0,0.88,0),(0,0,0.88)]
+# numtrials = len(b_color)
