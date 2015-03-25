a3c5040fe76bae7e2be8514307b957d63726e676
Jose Luis <jl.diez.antich@gmail.com>
2015-03-25 17:49:15 +0100

    trials_file: parameters ready for 3 trials

--- Experiment01 - Plaid/trials_file.py
+++ Experiment01 - Plaid/trials_file.py
@@ -1,22 +1,22 @@
-numtrials = 1 					# number of trials
+numtrials = 3 					# number of trials
 # parameters for gratings 1
 
 
 mylambda1 = (100,100,100)
-duty_cycle1 = 0.2
-speed1 = 70
-orientation1 = (8,10,12)
+duty_cycle1 = (0.2, 0.2, 0.2)
+speed1 = (60,70,80)
+orientation1 = (8,8,8)
 
 # parameters for gratings 2
 
 
 mylambda2 = (100,100,100)
-duty_cycle2 = 0.2
-speed2 = -70
-orientation2 = (-8,-10,-12)
+duty_cycle2 = (0.2,0.2,0.2)
+speed2 = (-60,-70,-80)
+orientation2 = (-8,-8,-8)
 
 #other parameters
-timeCurrentTrial = 5 	# time for each trial
+timeCurrentTrial = 30 	# time for each trial
 
 # b_color = [(0.0,0.88,0.88),(0.88,0,0),(0,0.88,0),(0,0,0.88)]
 # numtrials = len(b_color)
