78a07b9479135d6c3b257647b1ebbac0e5e5ef31
Jose Luis <jl.diez.antich@gmail.com>
2015-04-28 16:28:47 +0200

    comparingfilters: fix several issues
    Fix figure 4 bug, now it shows the correct values for snr and cv. Setting parameters for the different filters is now easier.

--- _Analysis/comparingfilters.py
+++ _Analysis/comparingfilters.py
@@ -43,13 +43,13 @@ def main():
 	# Need to set the window length initial value (odd integer), the initial value of polynomial order, 
 	# and the number of windows to compute.
 
-	wl_init 		= 3												# set initial value for window length. must be an odd integer
-	po_init 		= 2 											# set initial value for polynomial order
-	number_of_wins 	= 10 											# set number of windows
+	# define parameters
+	wl_array 	= [5, 7, 9, 11] 									# array of window lengths
+	po_for_wl 	= 3 	 											# polynomial order used with different window lengths
+	po_array 	= [2,3,4]			 								# array of polynomial orders
+	wl_for_po 	= 2 * np.floor(np.array(po_array) / 2) + 1 			# array of window lengths to use with different polynomial order values
 
-	vfwl_dict, vfpo_dict = savgol_plot(velocity, 					# filter velocity
-		wl_init, number_of_wins, po_init) 		
-
+	savgol_plot(velocity, wl_array, po_for_wl, po_array, wl_for_po) 		
 
 	# Differential filter. -----------------------------------------------------------------------------------------
 	# Parameters: bins and divisor.
@@ -58,11 +58,14 @@ def main():
 	# For different divisors, bins_init is used as the fixed bins value.
 	# Need to set the initial and maximum values for bins and divisor
 
-	bins_init, bins_max = 2, 10 									# set initial and maximum values for bins
-	dvsr_init, dvsr_max	= 2, 10 									# set initial and maximum values for divisor
+	# define parameters
+	bins_array 		= [2] 											# array of bins values
+	dvsr_for_bins 	= 3 											# divisor value used with different bins values
+	dvsr_array 		= [3,4] 										# array of divisors
+	bins_for_dvsr 	= 2 											# bins used with different divisors
 
-	vfbins_dict, vfdvsr_dict = differential_plot(data_deg, 			# filter data
-		velocity, bins_init, bins_max, dvsr_init, dvsr_max)
+	differential_plot(data_deg, velocity, bins_array, 				# filter data
+		dvsr_for_bins, dvsr_array, bins_for_dvsr)
 
 
 	plt.show() 														# show plots
@@ -84,7 +87,11 @@ def rgb2hex(color):
 
 	return "#{0:02x}{1:02x}{2:02x}".format(r, g, b)
 
-def savgol_plot(velocity, wl_init, number_of_wins, po_init):
+def savgol_plot(velocity, wl_array, po_for_wl, po_array, wl_for_po):
+	# compute snr and cv to velocity
+	v_snr = 10 * np.log10(scipy.stats.signaltonoise(velocity))	# compute signal to noise ratio
+	v_cv = scipy.stats.variation(velocity)        				# compute coefficient of variation
+	
 	# apply Savitzky-Golay filter
 	vfilt_wl = [] 													# initialize: velocity filtered w/ window length
 	vfilt_wl_snr = [] 												# to store snr values of v filtered w/ window length
@@ -94,15 +101,10 @@ def savgol_plot(velocity, wl_init, number_of_wins, po_init):
 	vfilt_po_snr = [] 												# to store snr values of v filtered w/ polyorder
 	vfilt_po_cv = [] 												# to store cv values of v filtered w/ polyorder
 
-	# initial parameters
-	window_length_array = np.arange(wl_init, number_of_wins, 2)		# compute array of window lengths
-	polyorder 	  		= po_init									# poly order must be smaller than window_length
-	polyorder_array 	= np.arange(polyorder, number_of_wins)		# compute array of polyorder
-
 	# filter velocity with different window lengths ---------------------------------------------------------------
-	for window_length in window_length_array:
+	for window_length in wl_array:
 		vfwl = signal.savgol_filter(velocity,  						# filter velocity with different window lengths
-			window_length, polyorder) 								# vfwl: velocity filtered window length
+			window_length, po_for_wl) 								# vfwl: velocity filtered window length
 
 		vf_snr 	= 10 * np.log10(scipy.stats.signaltonoise(vfwl))	# compute signal to noise ratio
 		vf_cv 	= scipy.stats.variation(vfwl)        				# compute coefficient of variation
@@ -110,14 +112,16 @@ def savgol_plot(velocity, wl_init, number_of_wins, po_init):
 		vfilt_wl.append(vfwl) 										# add filtered signal to array
 		vfilt_wl_snr.append(vf_snr) 								# add snr to array
 		vfilt_wl_cv.append(vf_cv) 									# add cv to array
-	
+
 	# filter velocity with different poly orders ------------------------------------------------------------------
 	new_window_length_array = []
-	for polyorder, idx in itertools.izip(polyorder_array, range(len(polyorder_array))):
+	for polyorder, window_length in itertools.izip(po_array, wl_for_po):
 
-		window_length = window_length_array[np.floor(idx/2)]
-		if window_length <= polyorder: window_length += 2
-		new_window_length_array.append(window_length)
+		if window_length <= polyorder:								#
+			print 'window length must be odd greater than polyorder. wl = {0}, po = {1}'.format(window_length, polyorder)#
+			window_length = int(2 * np.floor(polyorder/2) + 1)		#
+			if window_length == polyorder: window_length += 2
+			print 'Using window length = {0}'.format(window_length) #
 
 		vfpo = signal.savgol_filter(velocity,  						# filter velocity with different polynomial orders
 			window_length, polyorder) 								# vfpo: velocity filtered polynomial order
@@ -131,20 +135,20 @@ def savgol_plot(velocity, wl_init, number_of_wins, po_init):
 
 	# plot --------------------------------------------------------------------------------------------------------------
 	f1 = plt.figure(1)
-	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg)')
+	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg) SNR={0}dB.CV={1}'.format("%.2f" % v_snr,"%.2f" % v_cv))
 
-	for savgol_set, i, snr, cv in itertools.izip(vfilt_wl,window_length_array, vfilt_wl_snr, vfilt_wl_cv):
-		plt.plot(np.arange(len(savgol_set)), savgol_set, label = 'wl={0}.SNR={1}dB.CV={2}'.format(i,"%.2f" % snr,cv))
+	for savgol_set, i, snr, cv in itertools.izip(vfilt_wl, wl_array, vfilt_wl_snr, vfilt_wl_cv):
+		plt.plot(np.arange(len(savgol_set)), savgol_set, label = 'wl={0}.SNR={1}dB.CV={2}'.format(i,"%.2f" % snr,"%.2f" % cv))
 
 	plt.legend(loc='upper right')
 	plt.title('Velocity filtered by Savitzky-Golay. Different window lengths. polyorder = 2')		
 	plt.draw()
 
 	f2 = plt.figure(2)
-	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg)')
+	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg) SNR={0}dB.CV={1}'.format("%.2f" % v_snr,"%.2f" % v_cv))
 
-	for savgol_set, i, w, snr, cv in itertools.izip(vfilt_po, polyorder_array, new_window_length_array, vfilt_po_snr, vfilt_po_cv):
-		plt.plot(np.arange(len(savgol_set)), savgol_set, label = 'wl={0},po={1}.SNR={2}dB.CV={3}'.format(w,i,"%.2f" % snr,cv))
+	for savgol_set, i, w, snr, cv in itertools.izip(vfilt_po, po_array, wl_for_po, vfilt_po_snr, vfilt_po_cv):
+		plt.plot(np.arange(len(savgol_set)), savgol_set, label = 'wl={0},po={1}.SNR={2}dB.CV={3}'.format(w,i,"%.2f" % snr,"%.2f" % cv))
 
 	plt.legend(loc='upper right')			
 	plt.title('Velocity filtered by Savitzky-Golay. Different polyorder'.format())		
@@ -153,13 +157,16 @@ def savgol_plot(velocity, wl_init, number_of_wins, po_init):
 	# plt.show()
 
 	# return 
-	vfwl_dict = {'windows lengths': window_length_array, 'polyorder': po_init, 'SNR': vfilt_wl_snr, 'CV': vfilt_wl_cv}
-	vfpo_dict = {'windows lengths': new_window_length_array, 'polyorders': polyorder_array, 'SNR': vfilt_po_snr, 'CV': vfilt_po_cv}
+	vfwl_dict = {'windows lengths': wl_array, 'polyorder': po_for_wl, 'SNR': vfilt_wl_snr, 'CV': vfilt_wl_cv}
+	vfpo_dict = {'windows lengths': wl_for_po, 'polyorder': po_array, 'SNR': vfilt_po_snr, 'CV': vfilt_po_cv}
 
 	return vfwl_dict, vfpo_dict
 
-def differential_plot(data_deg, velocity, bins_init, bins_max, dvsr_init, dvsr_max):
-	
+def differential_plot(data_deg, velocity, bins_array, dvsr_for_bins, dvsr_array, bins_for_dvsr):
+	# compute snr and cv to velocity
+	v_snr = 10 * np.log10(scipy.stats.signaltonoise(velocity))	# compute signal to noise ratio
+	v_cv  = scipy.stats.variation(velocity)        				# compute coefficient of variation
+
 	# initialize arrays
 	diff_bins 		= []											# initialize array for different bins
 	diff_bins_snr 	= []
@@ -169,13 +176,8 @@ def differential_plot(data_deg, velocity, bins_init, bins_max, dvsr_init, dvsr_m
 	diff_dvsr_snr 	= []
 	diff_dvsr_cv 	= []
 
-	# initial parameters
-	bins_array 	= np.arange(bins_init, bins_max, 1) 				# array of bins number
-	dvsr_array	= np.arange(dvsr_init, dvsr_max, 1) 				# array of divisor numbers
-
-
 	# apply differential filter -----------------------------------
-	divisor_fixed = dvsr_init
+	divisor_fixed = dvsr_for_bins
 	for bins in bins_array:
 		vfb = differentialsmoothing(data_deg, bins, divisor_fixed) 	# filter data with different bins
 
@@ -186,12 +188,12 @@ def differential_plot(data_deg, velocity, bins_init, bins_max, dvsr_init, dvsr_m
 		diff_bins_snr.append(vf_snr) 								# add snr
 		diff_bins_cv.append(vf_cv) 									# add cv
 
-	bins_fixed = bins_init
+	bins_fixed = bins_for_dvsr
 	for dvsr in dvsr_array:
 		vfd = differentialsmoothing(data_deg, bins_fixed, dvsr) 	# filter data with different divisors
 
-		vf_snr 	= 10 * np.log10(scipy.stats.signaltonoise(vfb))		# compute signal to noise ratio
-		vf_cv 	= scipy.stats.variation(vfb)        				# compute coefficient of variation
+		vf_snr 	= 10 * np.log10(scipy.stats.signaltonoise(vfd))		# compute signal to noise ratio
+		vf_cv 	= scipy.stats.variation(vfd)        				# compute coefficient of variation
 
 		diff_dvsr.append(vfd)
 		diff_dvsr_snr.append(vf_snr) 								# add snr
@@ -199,17 +201,21 @@ def differential_plot(data_deg, velocity, bins_init, bins_max, dvsr_init, dvsr_m
 
 	# plot --------------------------------------------------------
 	f1 = plt.figure(3)
-	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg)')
+	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg) SNR={0}dB.CV={1}'.format("%.2f" % v_snr,"%.2f" % v_cv))
+
 	for v_filt, i, snr, cv in itertools.izip(diff_bins,bins_array,diff_bins_snr,diff_bins_cv):
-		plt.plot(np.arange(len(v_filt)), v_filt, label = 'bins={0}.SNR={1}dB.CV={2}'.format(i,"%.2f" % snr,cv))
+		plt.plot(np.arange(len(v_filt)), v_filt, label = 'bins={0}.SNR={1}dB.CV={2}'.format(i,"%.2f" % snr,"%.2f" % cv))
+
 	plt.legend(loc='upper right')
 	plt.title('Velocity filtered by Differential filter. divisor = {0}'.format(divisor_fixed))		
 	plt.draw()
 
 	f2 = plt.figure(4)
-	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg)')
+	plt.scatter(np.arange(len(velocity)), velocity, label = 'velocity (deg) SNR={0}dB.CV={1}'.format("%.2f" % v_snr,"%.2f" % v_cv))
+
 	for v_filt, i, snr, cv in itertools.izip(diff_dvsr,dvsr_array,diff_dvsr_snr,diff_dvsr_cv):
-		plt.plot(np.arange(len(v_filt)), v_filt, label = 'dvsr={0}.SNR={1}dB.CV={2}'.format(i,"%.2f" % snr,cv))
+		plt.plot(np.arange(len(v_filt)), v_filt, label = 'dvsr={0}.SNR={1}dB.CV={2}'.format(i,"%.2f" % snr,"%.2f" % cv))
+	
 	plt.legend(loc='upper right')			
 	plt.title('Velocity filtered by Differential filter. bins = {0}'.format(bins_fixed))		
 	plt.draw()
