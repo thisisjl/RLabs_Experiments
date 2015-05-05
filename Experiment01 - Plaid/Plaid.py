444dfdf603997695bed5b15377302ad9ce40d091
Jose Luis <jl.diez.antich@gmail.com>
2015-05-05 15:44:47 +0200

    Plaid: will create data folder if it does not exist

--- Experiment01 - Plaid/Plaid.py
+++ Experiment01 - Plaid/Plaid.py
@@ -1,34 +1,34 @@
 """
-Creates a Plaid stimulus.
+    Creates a Plaid stimulus.
 
-Execution: 
-    $ python Plaid.py [optional name] 
-    The optional name will be in the ouput data files, 
-    if no specified "defaultsubjectname" will be used.
+    Execution: 
+        $ python Plaid.py [optional name] 
+        The optional name will be in the ouput data files, 
+        if no specified "defaultsubjectname" will be used.
 
-Input: 
-    config_file.txt, trials_file.txt 
-    (written in INI format, read using Python's ConfigParser module 
-    (https://docs.python.org/2/library/configparser.html))
+    Input: 
+        config_file.txt, trials_file.txt 
+        (written in INI format, read using Python's ConfigParser module 
+        (https://docs.python.org/2/library/configparser.html))
 
-Output: 
-    text file for button presses data and, if used, eyetracker data.
+    Output: 
+        text file for button presses data and, if used, eyetracker data.
 
-    Format of the button press (event) data: 7 + n columns, where n is the number of trials.
-    column 1: time stamp. Time when the event was recorded
-    column 2: event name. "InputEvent" for button presses, "TrialEvent" for start and end of trial.
-    column 3: event type. If InputEvent, "Mouse_DW" for mouse button press, "Mouse_UP" for mouse buttton release. 
-        If TrialEvent, number of trial.
-    column 4: event id. If InputEvent, number of the button pressed. If TrialEvent, START or END.
-    column 5: event code. Code used in analysis: 1 means LEFT button press, 
-        -1 LEFT button press. 4/-4 for RIGHT button. 8 is trial start, -8 is trial end.
-    column 6: event counter
-    column 7: parameters name. Name of parameters used
-    column 8 to 7+n: value of the parameter for each trial
-    
-    The eyetracker data contains 38 columns, 
-    the first one is the eyetracker time stamp (computed by its own clock), 
-    the rest of the colums are the different eye data that tobii offers in addition to the button press data.
+        Format of the button press (event) data: 7 + n columns, where n is the number of trials.
+        column 1: time stamp. Time when the event was recorded
+        column 2: event name. "InputEvent" for button presses, "TrialEvent" for start and end of trial.
+        column 3: event type. If InputEvent, "Mouse_DW" for mouse button press, "Mouse_UP" for mouse buttton release. 
+            If TrialEvent, number of trial.
+        column 4: event id. If InputEvent, number of the button pressed. If TrialEvent, START or END.
+        column 5: event code. Code used in analysis: 1 means LEFT button press, 
+            -1 LEFT button press. 4/-4 for RIGHT button. 8 is trial start, -8 is trial end.
+        column 6: event counter
+        column 7: parameters name. Name of parameters used
+        column 8 to 7+n: value of the parameter for each trial
+        
+        The eyetracker data contains 38 columns, 
+        the first one is the eyetracker time stamp (computed by its own clock), 
+        the rest of the colums are the different eye data that tobii offers in addition to the button press data.
 """
 
 import os, sys
@@ -103,12 +103,14 @@ def main(
   
 
     # Initialize variables for data file ----------------------------------------------------------------------
-    if not os.path.isdir('data'):                                           # if there is not a folder called 'data',
-        os.makedirs('data')                                                 # create it
     # Name of the data files
     eyetrackeroutput   = os.path.join(application_path, os.path.join('data',(ExpName + "-" + time.strftime("%y.%m.%d_%H.%M", time.localtime()) + "_" + subjectname + "_" + "eyetracker_data" + ".txt")))
     filename_data      = os.path.join(application_path, os.path.join('data',(ExpName + "-" + time.strftime("%y.%m.%d_%H.%M", time.localtime()) + "_" + subjectname + "_" + "button_press_data" + ".txt")))
     filename_transtns  = os.path.join(application_path, os.path.join('data',(ExpName + "-" + time.strftime("%y.%m.%d_%H.%M", time.localtime()) + "_" + subjectname + "_" + "transitions_file" + ".txt")))
+    # data folder
+    data_folder        = os.path.join(application_path, 'data')
+    if not os.path.isdir(data_folder):                                           # if there is not a folder called 'data',
+        os.makedirs(data_folder)                                                 # create it             
    
     # 3.4 - Initialize text to be shown at startup (not whown right now)
     textInstruc = "Continually report the motion of the grating in front.\nPress the left mouse button for left-ward motion.\nPress the right mouse button for right-ward motion\n\nClick mouse-wheel to start"
