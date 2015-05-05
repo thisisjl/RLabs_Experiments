da8e0ce79ccb57590872261b4afb5a8eea9e92f0
Jose Luis <jl.diez.antich@gmail.com>
2015-05-05 17:15:51 +0200

    libutils: DataStruct() does read parameters
    Parameters used in the experiment will be written in the data file, but they are not being read in the analysis. That is because i do not know how to use np.genfromtxt() to read columns that contain more an array (e.g. color parameters). Hirherto experiment parameters have not been used in the analysis, so it is not a problem at all.

--- _Libraries/rlabs_libutils.py
+++ _Libraries/rlabs_libutils.py
@@ -280,8 +280,8 @@ class DataStruct():
                 print '{0}: eyetracker data (34 colums format). This does not have parameters'.format(os.path.split(self.filename)[1])
             except ValueError:
                 try:
-                    data = np.genfromtxt(self.filenamefp, delimiter="\t",                           # if that stil does not work,
-                    dtype=None, names=True)                                                         # only button presses file
+                    data = np.genfromtxt(self.filenamefp, delimiter="\t",                           # if that stil does not work
+                    dtype=None, names=True, usecols = range(6))                                     # try with 6 colums (only button presses file, no parameters)
                     print '{0}: button press data'.format(os.path.split(self.filename)[1])
                 except ValueError:                                                                  # if neither of these work, 
                     print 'cannot read data file {0}'.format(os.path.split(self.filenamefp)[1])     # cannot read data file
