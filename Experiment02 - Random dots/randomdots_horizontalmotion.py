aac7ed0073104a63c07ec068d9f21da3e2188781
Jose Luis <jl.diez.antich@gmail.com>
2015-05-11 11:36:29 +0200

    RD horizontal motion: add eyetracker support

--- Experiment02 - Random dots/randomdots_horizontalmotion.py
+++ Experiment02 - Random dots/randomdots_horizontalmotion.py
@@ -1,9 +1,10 @@
 import os, sys
 
-lib_path = os.path.abspath(os.path.join('../..','_Libraries'))
+lib_path = os.path.abspath(os.path.join('..','_Libraries'))
 sys.path.append(lib_path)
 
 from rlabs_libutils import *
+from rlabs_libtobii import EyetrackerBrowser, MyTobiiController             			# this is OUR library for the tobii eyetracker
 
 from pyglet.window import Window, mouse
 from pyglet.gl import *                                                     			# to change the color of the background
@@ -12,39 +13,81 @@ import time
 import numpy as np
 
 
-def main(num_dots=100, Tau = 100):
-	win = MyWindow(fullscreen = True)
-	center = win.width/2, win.height/2
-	fg_color = (0,0,0)
+def main(num_dots=100, Tau = 100, haveeyetracker = 0):
+	
+	screens = pyglet.window.get_platform().get_default_display().get_screens()			# get number of screens
+	win = MyWindow(fullscreen = True, screen = screens[0], visible = 0)					# create window
+	center = win.width/2, win.height/2 													#
+	fg_color = (0,0,0) 																	#
 	clockDisplay = clock.ClockDisplay(color=(1,1,1,1))                      			# to display frames per second
-	framerate = 60.0
+	framerate = 60.0 																	#
 	clock.set_fps_limit(framerate)                                          			# set limit for frames per second
 
-	speed = 500 																			# pixels per second
-	motionpath = 1000 																	# maximum number of pixels each dot is going to move in its lifetime
+	parameters = []
+	speed = 500 																		# pixels per second
 	
-
 	# define limits
-	liml, limr = win.width/4, win.width - win.width/4
-	cyclestart, cycleend = 0, win.width
+	liml, limr = win.width/4, win.width - win.width/4 									#
+	cyclestart, cycleend = 0, win.width 												#
 
 	# compute dots position: liml < x < limr
-	x    = np.random.rand(num_dots) * win.width#* (limr - liml) + liml
-	y    = np.random.rand(num_dots) * win.height
-
-	z_fb = 2 * np.mod(np.random.permutation(num_dots),2) - 1
+	x = np.random.rand(num_dots) * win.width											#
+	y = np.random.rand(num_dots) * win.height 											#
+	z_fb = 2 * np.mod(np.random.permutation(num_dots),2) - 1 							#
 
 	vertices = np.empty((x.size + y.size,), dtype=x.dtype)               				# create empty array to allocate x and y coordinates
-	age = np.random.randint(Tau, size=num_dots)
+	age = np.random.randint(Tau, size=num_dots) 										#
 
 	# compute distance between x and cyclestart
-	dxcys = (x - liml) - cyclestart
-	cycle = cycleend - cyclestart
+	dxcys = (x - liml) - cyclestart 													#
+	cycle = cycleend - cyclestart 														#
 	
-	dx = np.empty(z_fb.size)
+	dx = np.empty(z_fb.size) 															#
 
+	
+	# handling events ---------------------------------------------------------------------------------------------------  
+	events_struct = []                                                      			# list that contains event that the event handler sends.
+	eventcount = 0                                                          			# counter of the events
+
+	events_handler = {                                                      			# events handler (it needs to be set to window)
+	    'on_mouse_press'    : lambda e: events_struct.append(e),            			# append on_mouse_press event to events_struct
+	    'on_mouse_release'  : lambda e: events_struct.append(e),}           			# append on_mouse_release event to events_struct
+
+	events_handler_with_ET = {                                                                              # if using eyetracker, this will add
+	    'on_mouse_press'    : lambda e: (events_struct.append(e), controller.myRecordEvent2(event = e)),    # to events_struct and also it will 
+	    'on_mouse_release'  : lambda e: (events_struct.append(e), controller.myRecordEvent2(event = e)),}   # call MyRecordEvent2 with the event
+
+
+    # prepare for eyetracker ----------------------------------------------------------------------------------------------
+	if haveeyetracker:
+		if not os.path.isdir('data'):                                           				# if there is not a folder called 'data',
+		    os.makedirs('data')                                                 				# create it
+		eyetrackeroutput   = os.path.join('data',("Randomdots" + "-" + time.strftime( 			# eyetracker data file name
+			"%y.%m.%d_%H.%M", time.localtime()) + "_" + subjectname + "_" + "eyet" + ".txt"))	#
+		eb = EyetrackerBrowser()																# Create an EyetrackerBrowser
+		eb.main()																				# display EyetrackerBrowser
+
+		controller = MyTobiiController( 														# create Tobii controller
+			datafilename=eyetrackeroutput, 														# pass it data file name
+			parameters=parameters)       														# pass it parameters
+		controller.waitForFindEyeTracker()                                  					# wait to find eyetracker
+		controller.activate(controller.eyetrackers.keys()[0])               					# activate eyetracker
+
+		controller.startTracking()                                                              # start the eye tracking recording
+		time.sleep(0.2)                                                                         # wait for the eytracker to warm up
+		controller.myRecordEvent2(EventItem(name = 'TrialEvent', counter = 0, 
+			timestamp = time.time(), etype = 0, eid = 'START'))
+
+		win.events_handler = events_handler_with_ET                       					# set window events_handler with eye tracker
+
+	else:
+		win.events_handler = events_handler                               					# set window events_handler
+
+	
 	# Stimulus loop  -------------------------------------------------------------------------------------------------------------
-	timestart = time.time()
+	win.set_visible(True) 																#
+	win.set_mouse_visible(False)                                          				# set mouse to not visible
+	timestart = time.time() 															#
 	while not win.has_exit:
 		glClearColor(fg_color[0],fg_color[1],fg_color[2],1)                 			# set background color
 		win.clear()                                                         			# clear window
@@ -57,10 +100,10 @@ def main(num_dots=100, Tau = 100):
 		y[newdots]    = np.random.rand(np.sum(newdots)) * win.height 					#
 
 		# update position --------------------------------------------------------------------------------------------------------
-		dividend = x + z_fb * (speed * (timenow - timestart))
-		dx[z_fb == 1] = np.fmod(dividend[z_fb == 1], cycle)
-		dx[z_fb == -1] = cycleend - np.fmod(cycleend - dividend[z_fb == -1], cycle)
-		dotxpos = dx
+		dividend       = x + z_fb * (speed * (timenow - timestart))  					#
+		dx[z_fb == 1]  = np.fmod(dividend[z_fb == 1], cycle) 							#
+		dx[z_fb == -1] = cycleend - np.fmod(cycleend - dividend[z_fb == -1], cycle) 	#
+		dotxpos        = dx
 
 		# Update ages -------------------------------------------------------------------------------------------------------------
 		age          = age - 1 															#
@@ -71,9 +114,15 @@ def main(num_dots=100, Tau = 100):
 		vertices[1::2] = y																# y coords will be in odd indices
 		drawpoints(vertices, color = (255,255,255), size = 5)  							# draw dots
 
-		clock.tick()
+		clock.tick() 																	#
 		clockDisplay.draw()                                                 			# display frames per second
 		win.flip()                                                          			# flip window
+
+	if haveeyetracker:															# Stop eyetracker processes
+		controller.myRecordEvent2(EventItem(name = 'TrialEvent', counter = 1, 
+			timestamp = time.time(), etype = 0, eid = 'END'))
+		controller.stopTracking()                                           			# stop eye tracking and write output file
+		controller.destroy()                                                			# destroy controller
 
 if __name__ == '__main__':
     main()
