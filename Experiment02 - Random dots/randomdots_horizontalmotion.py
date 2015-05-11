041569b10ff1a8074dd3810f588d8f5746d77132
Jose Luis <jl.diez.antich@gmail.com>
2015-05-11 12:50:13 +0200

    RD horizontal motion: add manual frame control
    For each frame, get start and end time stamps in the while loop. The difference <end-start> is the time that took to perform the computations of the frame. The difference <1.0/framerate - <end-start>> is the time the frame must be frozen. The frame will be frozen using time.sleep()

--- Experiment02 - Random dots/randomdots_horizontalmotion.py
+++ Experiment02 - Random dots/randomdots_horizontalmotion.py
@@ -23,6 +23,8 @@ def main(num_dots=100, Tau = 100, haveeyetracker = 0):
 	framerate = 60.0 																	#
 	clock.set_fps_limit(framerate)                                          			# set limit for frames per second
 
+	frameMs = 1.0/framerate                                        						# manual frame rate control: frameMs is the time in ms a frame will be displayed
+
 	parameters = []
 	speed = 500 																		# pixels per second
 	
@@ -37,7 +39,7 @@ def main(num_dots=100, Tau = 100, haveeyetracker = 0):
 
 	vertices = np.empty((x.size + y.size,), dtype=x.dtype)               				# create empty array to allocate x and y coordinates
 	age = np.random.randint(Tau, size=num_dots) 										#
-
+	age[0] = 10000
 	# compute distance between x and cyclestart
 	dxcys = (x - liml) - cyclestart 													#
 	cycle = cycleend - cyclestart 														#
@@ -81,7 +83,7 @@ def main(num_dots=100, Tau = 100, haveeyetracker = 0):
 		win.events_handler = events_handler_with_ET                       					# set window events_handler with eye tracker
 
 	else:
-		win.events_handler = events_handler                               					# set window events_handler
+		win.events_handler = events_handler                               				# set window events_handler
 
 	
 	# Stimulus loop  -------------------------------------------------------------------------------------------------------------
@@ -103,12 +105,12 @@ def main(num_dots=100, Tau = 100, haveeyetracker = 0):
 		dividend       = x + z_fb * (speed * (timenow - timestart))  					#
 		dx[z_fb == 1]  = np.fmod(dividend[z_fb == 1], cycle) 							#
 		dx[z_fb == -1] = cycleend - np.fmod(cycleend - dividend[z_fb == -1], cycle) 	#
-		dotxpos        = dx
+		dotxpos        = dx 															#
 
 		# Update ages -------------------------------------------------------------------------------------------------------------
 		age          = age - 1 															#
 		age[age==-1] = Tau - 1  														# 
-
+		
 		# Draw dots ---------------------------------------------------------------------------------------------------------------
 		vertices[0::2] = dotxpos														# x coords will be in even indices
 		vertices[1::2] = y																# y coords will be in odd indices
@@ -117,8 +119,14 @@ def main(num_dots=100, Tau = 100, haveeyetracker = 0):
 		clock.tick() 																	#
 		clockDisplay.draw()                                                 			# display frames per second
 		win.flip()                                                          			# flip window
+		
+		# manual frame control ----------------------------------------------------------------------------------------------------
+		endMs = time.time() 															# manual frame rate control: time point when frame ends.
+		delaytime = frameMs - (endMs - timenow) 										# manual frame rate control: time time frame must be frozen.
+		if delaytime > 0: time.sleep(delaytime)											# manual frame rate control: freeze frame
 
-	if haveeyetracker:															# Stop eyetracker processes
+
+	if haveeyetracker:																	# Stop eyetracker processes
 		controller.myRecordEvent2(EventItem(name = 'TrialEvent', counter = 1, 
 			timestamp = time.time(), etype = 0, eid = 'END'))
 		controller.stopTracking()                                           			# stop eye tracking and write output file
