6e042e1016b75ac888b204d003ecfa6b8a40e9f5
Jose Luis <jl.diez.antich@gmail.com>
2015-06-12 12:48:08 +0200

    show_buttonpress_over eye_outliers: add filter outliers
    two figures will be created, first one is the same as before, second one compares the outliers with the filtered outliers.

--- _Analysis/show_buttonpress_over_eye_outliers.py
+++ _Analysis/show_buttonpress_over_eye_outliers.py
@@ -1,7 +1,7 @@
 import os, sys
 lib_path = os.path.abspath(os.path.join('..','_Libraries'))
 sys.path.append(lib_path)
-from rlabs_libutils import DataStruct, select_data, gentimeseries, gencontinuousoutliers
+from rlabs_libutils import DataStruct, select_data, gentimeseries, gencontinuousoutliers, filteroutliers
 import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import izip
@@ -38,8 +38,8 @@ df['velocity'] = df['LEpos_int'].diff() * framerate
 df['isOutlier'] = abs(df['velocity'] - df['velocity'].mean()) > 1.96*df['velocity'].std()
 
 # create two new DataFrames: for outliers and non-outliers:
-outliers = df[df['Outlier']].dropna()
-nonoutliers = df[~df['Outlier']].dropna()
+outliers = df[df['isOutlier']].dropna()
+nonoutliers = df[~df['isOutlier']].dropna()
 
 # compute A or B given outliers
 df['A percept'] = [v>0 if o else 0 for v,o in izip(df['velocity'],df['isOutlier'])]
@@ -49,13 +49,23 @@ df['B percept'] = [v<0 if o else 0 for v,o in izip(df['velocity'],df['isOutlier'
 df['A percept continuous'] = gencontinuousoutliers(df['A percept'],df['B percept'])
 df['B percept continuous'] = gencontinuousoutliers(df['B percept'],df['A percept'])
 
+# filter outliers
+# 	so outliers will only be valid if are separated from previous outliers
+# 	by a X time distance.
+# 	compute time:
+mintime = 0.3 			# seconds
+samples = framerate * mintime
+df['Outlierfiltered'] = filteroutliers(df['isOutlier'], samples = samples)
+
+# create two new DataFrames: for filtered outliers and filtered non-outliers:
+filtoutliers = df[df['Outlierfiltered']].dropna()
+filtnonoutliers = df[~df['Outlierfiltered']].dropna()
+
 # generate percept time series (button presses)
 df['A press'] = gentimeseries(ds.timestamps, ds.A_ts)
 df['B press'] = gentimeseries(ds.timestamps, ds.B_ts)
 
-
 # plot it --------------------------------------------------------------------------------
-
 ax1 = plt.subplot(4, 1, 1)
 ax1.plot(df['time'], df['LEpos_int'], label = 'leftgazeX (interpolated)')
 
@@ -80,4 +90,27 @@ ax4.set_ylim([0, 1.05])
 ax4.legend()
 
 plt.xlabel('time (s)')
+
+# second figure ----------------------------------------
+fig2 = plt.figure()
+ax5 = plt.subplot(3, 1, 1)
+ax5.plot(df['time'], df['LEpos_int'], label = 'leftgazeX (interpolated)')
+
+ax6 = plt.subplot(3, 1, 2, sharex = ax5)
+ax6.scatter(outliers['time'], outliers['velocity'], color = 'r', label='outliers')
+ax6.scatter(nonoutliers['time'], nonoutliers['velocity'], color ='g', label='non-outliers') 
+# ax2.set_title('Velocity with outliers determined 1.96 * SD')
+ax6.set_ylim()
+ax6.legend()
+
+ax7 = plt.subplot(3, 1, 3, sharex = ax5)
+ax7.scatter(filtoutliers['time'], filtoutliers['velocity'], color = 'r', label='filt-outliers')
+ax7.scatter(filtnonoutliers['time'], filtnonoutliers['velocity'], color ='g', label='filt-non-outliers') 
+# ax2.set_title('Velocity with outliers determined 1.96 * SD')
+ax7.set_ylim()
+ax7.legend()
+
+plt.xlabel('time (s)')
+
+
 plt.show()
